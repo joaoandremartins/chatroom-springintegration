a0bafe4e429e6b460445efc375f92bc3d2baf954
João André Martins <joaomartins@google.com>
2017-05-12 16:43:22 -0400

    Client app with PubSub channel adapter.

--- src/main/java/com/google/springongcp/pubsub/PubSubApplication.java
+++ src/main/java/com/google/springongcp/pubsub/PubSubApplication.java
@@ -1,42 +1,97 @@
 package com.google.springongcp.pubsub;
 
+import com.google.cloud.pubsub.spi.v1.Publisher;
 import com.google.springongcp.model.LoggableMessage;
-import java.time.LocalDateTime;
 import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.integration.annotation.Publisher;
-import org.springframework.integration.googlepubsub.channel.Subscriber;
-import org.springframework.messaging.Message;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.googlepubsub.channel.inbound.PubSubInboundChannelAdapter;
+import org.springframework.integration.googlepubsub.channel.outbound.PubSubMessagingTemplate;
+import org.springframework.integration.googlepubsub.channel.outbound.PubSubOutboundChannelAdapter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 /**
  * Created by joaomartins on 5/3/17.
  */
 @SpringBootApplication
+@RestController
+@ComponentScan(basePackages = {"org.springframework.integration.googlepubsub.channel"})
 public class PubSubApplication {
 
   private static final Logger LOGGER = Logger.getGlobal();
+  private MessageChannel outputChannel = new DirectChannel();
+
+  @Autowired
+  private Publisher publisher;
+  @Autowired
+  private PubSubMessagingTemplate pubsubTemplate;
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(PubSubApplication.class, args);
   }
 
-  @Publisher(channel = "topic")
-  public Object sendMessage() {
-    return new LoggableMessage("joao", "Hello world!", LocalDateTime.now());
+  @Bean
+  public MessageChannel pubsubInputChannel() {
+    return new DirectChannel();
   }
 
-  @Subscriber(topic = "topic")
-  public void receiveMessage(Message<?> message) {
-    LOGGER.info("Message arrived!");
-    if (message.getPayload() instanceof LoggableMessage) {
-      LOGGER.info(((LoggableMessage) message.getPayload()).getBody());
-    }
+  @Bean
+  public PubSubInboundChannelAdapter inboundChannelAdapter(
+      @Qualifier("pubsubInputChannel") MessageChannel inputChannel) {
+    PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter();
+    adapter.setOutputChannel(inputChannel);
+
+    return adapter;
   }
 
-  @Subscriber(topic = "topic")
-  public void receiveMessageInParallel(Message<?> message) {
-    LOGGER.info("Message also arrived here!");
+  @Bean
+  @ServiceActivator(inputChannel = "pubsubInputChannel")
+  public MessageHandler receiveMessage() {
+    return message -> {
+      LOGGER.info("Message arrived!");
+      if (message.getPayload() instanceof LoggableMessage) {
+        LOGGER.info(((LoggableMessage) message.getPayload()).getBody());
+      }
+    };
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = "pubsubInputChannel")
+  public MessageHandler receiveMessageInParallel() {
+    return message -> LOGGER.info("Message also arrived here!");
+  }
+
+  @PostMapping("/postMessage")
+  public RedirectView addMessage(@RequestParam("message") String message) {
+    pubsubTemplate.convertAndSend(outputChannel, message);
+    return new RedirectView("/");
+  }
+
+  @Bean
+  @ServiceActivator(inputChannel = "pubsubOutputChannel")
+  public MessageHandler messageSender() {
+    return new PubSubOutboundChannelAdapter(pubSubMessagingTemplate());
+  }
+
+  @Bean
+  public MessageChannel pubsubOutputChannel() {
+    return outputChannel;
+  }
+
+  @Bean
+  public PubSubMessagingTemplate pubSubMessagingTemplate() {
+    return new PubSubMessagingTemplate(publisher);
   }
 }
